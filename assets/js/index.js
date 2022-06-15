5fea1b733aedb2e6a47186b1726eae65be195a22
devgustavosantos <devgustavosantos@gmail.com>
2022-06-15 10:21:57 -0300

    refactor: sound name change

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+const buttonPlay = document.getElementById("play");
+const buttonStop = document.getElementById("stop");
+const buttonIncressed = document.getElementById("increase");
+const buttonDecrease = document.getElementById("decrease");
+
+const minutesDisplay = document.getElementById("minutes");
+const secondsDisplay = document.getElementById("seconds");
+
+const cardForest = document.getElementById("forest");
+const cardRain = document.getElementById("rain");
+const cardCoffeeShop = document.getElementById("coffee-shop");
+const cardFirePit = document.getElementById("fire-pit");
+
