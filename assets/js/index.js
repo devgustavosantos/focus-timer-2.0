4fac5cb196792803b22eaf1b3935bd119f8e002b
devgustavosantos <devgustavosantos@gmail.com>
2022-06-15 15:29:25 -0300

    feat: added countdown function

--- assets/js/index.js
+++ assets/js/index.js
@@ -15,7 +15,40 @@ const soundForest = new Audio("./assets/sound/forest.wav");
 const soundRain = new Audio("./assets/sound/rain.wav");
 const soundCoffeeShop = new Audio("./assets/sound/coffee-shop.wav");
 const soundFirePit = new Audio("./assets/sound/fire-pit.wav");
+const alertTimer = new Audio("./assets/sound/alert.mp3");
 
+let timer;
+let timeTimerOut = 3;
+let countInProgress = false;
+
+
+function decreaseCount() {
+        timer = setTimeout(function() {
+            let seconds = Number(secondsDisplay.textContent);
+            let minutes = Number(minutesDisplay.textContent);
+            let isTimerOver = seconds <= 0 && minutes <= 0;
+    
+            if(isTimerOver) {
+                alertTimer.play();
+                resetTimeDisplay();
+                countInProgress = false;
+                return;
+            }
+
+            if(seconds <= 0) {
+                minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
+                secondsDisplay.textContent = String(timeTimerOut).padStart(2, "0");
+            } else {
+                secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
+            }
+            decreaseCount();
+        }, 1000);
+}
+
+function resetTimeDisplay() {
+    minutesDisplay.textContent = "25";
+    secondsDisplay.textContent = "00";
+}
 
 function soundTurnOn(sound) {
     sound.loop = true;
@@ -32,18 +65,41 @@ function activatedCard(card){
     card.classList.toggle("sound-on-div");
 }
 
+buttonPlay.addEventListener("click", function() {
+    if(!countInProgress) {
+        decreaseCount();
+        countInProgress = true;
+    }
+})
+  
+buttonStop.addEventListener("click", function() {
+    if(countInProgress) {
+        clearTimeout(timer);
+        resetTimeDisplay();
+        countInProgress = false;
+    }
+})
 
 buttonIncresse.addEventListener("click", function() {
-    let minutes = Number(minutesDisplay.textContent)
-    minutesDisplay.textContent = String(minutes + 5);
+    let minutes = Number(minutesDisplay.textContent);
+    let seconds = Number(secondsDisplay.textContent);
+    let maximumTimeReached = minutes >= 95;
+    
+    if(maximumTimeReached) {
+        minutesDisplay.textContent = "99";
+    } else {
+        minutesDisplay.textContent = String(minutes + 5).padStart(2, "0");
+    }
 })
 
 buttonDecrease.addEventListener("click", function() {
-    let minutes = Number(minutesDisplay.textContent)
-    if(minutes >= 5) {
-        minutesDisplay.textContent = String(minutes - 5);
+    let minutes = Number(minutesDisplay.textContent);
+    let seconds = Number(secondsDisplay.textContent);
+    let minimumTimeReached = (minutes > 5) || (minutes == 5 && seconds > 0);
+    if(minimumTimeReached) {
+        minutesDisplay.textContent = String(minutes - 5).padStart(2, "0");
     } else {
-        alert("Não é possivel diminuir o tempo em 5 minutos!")
+        alert("Não é possivel diminuir o tempo em 5 minutos!");
     }
 })
 
