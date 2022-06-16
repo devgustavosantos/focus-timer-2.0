9adf4dd3530ba30b52fefc5590c7047ebac12385
devgustavosantos <devgustavosantos@gmail.com>
2022-06-16 11:16:04 -0300

    refactor: application of clean code concepts before modularization

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+//Elements-------------------------------------------
 const buttonPlay = document.getElementById("play");
 const buttonStop = document.getElementById("stop");
 const buttonIncresse = document.getElementById("increase");
@@ -11,45 +12,100 @@ const cardRain = document.getElementById("rain");
 const cardCoffeeShop = document.getElementById("coffee-shop");
 const cardFirePit = document.getElementById("fire-pit");
 
+
+// Timer---------------------------------------------------------
+let timer;
+let timeTimerOut = 59;
+let standardMinutes = minutesDisplay;
+let countInProgress = false;
+
+function decreaseCount() {
+    timer = setTimeout(function() {
+        let seconds = Number(secondsDisplay.textContent);
+        let minutes = Number(minutesDisplay.textContent);
+        let isTimerOver = seconds <= 0 && minutes <= 0;
+
+        if(isTimerOver) {
+            soundAlert.play();
+            updateTime(standardMinutes, 0);
+            countInProgress = false;
+            return;
+        }
+
+        if(seconds <= 0) {
+            updateTime((minutes - 1), timeTimerOut);
+        } else {
+            updateTime(minutes, (seconds - 1));
+        }
+
+        decreaseCount();
+    }, 1000);
+}
+
+function startCounting() {
+    if(!countInProgress) {
+        decreaseCount();
+        countInProgress = true;
+    } else {
+        alert("Não é possivel iniciar o temporizador, pois ele já está em andamento!");
+    }
+}
+
+function stopCounting() {
+    if(countInProgress) {
+        clearTimeout(timer);
+        updateTime(standardMinutes, 0);
+        countInProgress = false;
+    } else {
+        alert("Não é possivel parar o temporizador o temporizador, pois ele já foi interrompido!");
+    }
+}
+
+
+// Display ---------------------------------------------------------------------------
+function updateTime(minutes, seconds) {
+    minutesDisplay.textContent = String(minutes).padStart(2, "0");
+    secondsDisplay.textContent = String(seconds).padStart(2, "0");
+}
+
+function userIncreasedTime() {
+    let minutes = Number(minutesDisplay.textContent);
+    let seconds = Number(secondsDisplay.textContent);
+    let maximumTimeReached = minutes >= 95;
+    
+    if(maximumTimeReached) {
+        updateTime(99, 59);
+        alert("O tempo máximo do temporizador é de 99:59!");
+
+    } else {
+        updateTime((minutes + 5), seconds);
+    }
+}
+
+function userDecreasedTime() {
+    let minutes = Number(minutesDisplay.textContent);
+    let seconds = Number(secondsDisplay.textContent);
+    let minimumTimeReached = (minutes > 5) || (minutes == 5 && seconds > 0);
+    
+    if(minimumTimeReached) {
+        updateTime((minutes - 5), seconds);
+    } else {
+        alert("Não é possivel diminuir o tempo em 5 minutos!");
+    }
+}
+
+function activatedCard(card){
+    let icon = card.children[0].children[0];
+    icon.classList.toggle("sound-on-svg");
+    card.classList.toggle("sound-on-div");
+}
+
+// Sounds ----------------------------------------------------------------------------------
 const soundForest = new Audio("./assets/sound/forest.wav");
 const soundRain = new Audio("./assets/sound/rain.wav");
 const soundCoffeeShop = new Audio("./assets/sound/coffee-shop.wav");
 const soundFirePit = new Audio("./assets/sound/fire-pit.wav");
-const alertTimer = new Audio("./assets/sound/alert.mp3");
-
-let timer;
-let timeTimerOut = 59;
-let countInProgress = false;
-
-
-function decreaseCount() {
-        timer = setTimeout(function() {
-            let seconds = Number(secondsDisplay.textContent);
-            let minutes = Number(minutesDisplay.textContent);
-            let isTimerOver = seconds <= 0 && minutes <= 0;
-    
-            if(isTimerOver) {
-                alertTimer.play();
-                resetTimeDisplay();
-                countInProgress = false;
-                return;
-            }
-
-            if(seconds <= 0) {
-                minutesDisplay.textContent = String(minutes - 1).padStart(2, "0");
-                secondsDisplay.textContent = String(timeTimerOut).padStart(2, "0");
-            } else {
-                secondsDisplay.textContent = String(seconds - 1).padStart(2, "0");
-            }
-
-            decreaseCount();
-        }, 1000);
-}
-
-function resetTimeDisplay() {
-    minutesDisplay.textContent = "25";
-    secondsDisplay.textContent = "00";
-}
+const soundAlert = new Audio("./assets/sound/alert.mp3");
 
 function soundTurnOn(sound) {
     sound.loop = true;
@@ -60,48 +116,22 @@ function soundTurnOn(sound) {
     }
 }
 
-function activatedCard(card){
-    let icon = card.children[0].children[0];
-    icon.classList.toggle("sound-on-svg");
-    card.classList.toggle("sound-on-div");
-}
 
+// Index ----------------------------------------------------------------------------
 buttonPlay.addEventListener("click", function() {
-    if(!countInProgress) {
-        decreaseCount();
-        countInProgress = true;
-    }
+   startCounting();
 })
   
 buttonStop.addEventListener("click", function() {
-    if(countInProgress) {
-        clearTimeout(timer);
-        resetTimeDisplay();
-        countInProgress = false;
-    }
+   stopCounting();
 })
 
 buttonIncresse.addEventListener("click", function() {
-    let minutes = Number(minutesDisplay.textContent);
-    let seconds = Number(secondsDisplay.textContent);
-    let maximumTimeReached = minutes >= 95;
-    
-    if(maximumTimeReached) {
-        minutesDisplay.textContent = "99";
-    } else {
-        minutesDisplay.textContent = String(minutes + 5).padStart(2, "0");
-    }
+    userIncreasedTime();
 })
 
 buttonDecrease.addEventListener("click", function() {
-    let minutes = Number(minutesDisplay.textContent);
-    let seconds = Number(secondsDisplay.textContent);
-    let minimumTimeReached = (minutes > 5) || (minutes == 5 && seconds > 0);
-    if(minimumTimeReached) {
-        minutesDisplay.textContent = String(minutes - 5).padStart(2, "0");
-    } else {
-        alert("Não é possivel diminuir o tempo em 5 minutos!");
-    }
+    userDecreasedTime();
 })
 
 cardForest.addEventListener("click", function() {
@@ -123,3 +153,6 @@ cardFirePit.addEventListener("click", function() {
     soundTurnOn(soundFirePit);
     activatedCard(cardFirePit);
 })
+
+
+// testes -----------------------------------------------------
